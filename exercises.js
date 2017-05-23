404366ee2e4465ac09cf74d7ce20ea3dcee8ba82
djedynak259 <dajed@outlook.com>
2017-05-23 16:55:08 -0700

    3 and 5 multiple alternate

--- exercises.js
+++ exercises.js
@@ -351,3 +351,14 @@ function findSum(n) {
   return sum
 }
 
+  // Good Alternate
+
+  function findSum(n) {
+    let result = 0;
+    for (let i = 0; i <= n; i += 1) {
+      if (i % 3 ===0 || i % 5 === 0) result += i
+    }
+    return result
+  }
+
+
