b481cb8c20654f4b4709ac6f9ce28743312d1d6f
djedynak259 <dajed@outlook.com>
2017-05-25 15:37:20 -0700

    number compare

--- exercises.js
+++ exercises.js
@@ -486,3 +486,15 @@ function SimpleSymbols(str) {
     return true;
   }
 
+// Compare two numbers
+
+function CheckNums(num1,num2) { 
+    if (num2 > num1) {
+        return true
+    } else if (num1 === num2) {
+        return -1
+    } else {
+        return false
+    }
+}
+
