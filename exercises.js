03127d24cd3150fd0e10ad0ac92953fdb700912f
djedynak259 <dajed@outlook.com>
2017-06-02 16:36:49 -0700

    duplicates 2

--- exercises.js
+++ exercises.js
@@ -1258,3 +1258,37 @@ function duplicates(arr) {
 
 duplicates([0,2,0,1,3,3]);
 
+
+// Duplicates 2
+
+function duplicates(arr) {
+  
+  // our hash table to store each element
+  // in the array as we pass through it
+  var hashTable = [];
+  
+  // store duplicates
+  var dups = [];
+  
+  // check each element in the array
+  for (var i = 0; i < arr.length; i++) {
+    
+    // if element does not exist in hash table
+    // then insert it
+    if (hashTable[arr[i].toString()] === undefined) {
+      hashTable[arr[i].toString()] = true;
+    } 
+    
+    // if element does exist in hash table
+    // then we know it is a duplicate
+    else { dups.push(arr[i]); }
+    
+  }
+  
+  return dups;
+  
+}
+
+duplicates([1, 21, -4, 103, 21, 4, 1]);
+
+
