820c7492fc7e4dbec4ed4e5ea963e2fedbb6f667
djedynak259 <dajed@outlook.com>
2017-06-02 15:38:28 -0700

    find all string alternates with ?

--- exercises.js
+++ exercises.js
@@ -1184,3 +1184,43 @@ powerSet([1, 2, 3]);
 // [[], [1], [2], [3], [1, 2], [2, 3], [1, 3] [1, 2, 3]]
 
 
+
+// Find all string combinations including alternates with '?'
+
+function patterns(str, all) {
+  
+  // if the string is empty, return all the string sets
+  if (str.length === 0) { return all; }
+  
+  // if character is 0 or 1 then add the character to each
+  // string set we currently have so far
+  if (str[0] === '0' || str[0] === '1') {
+    for (var i = 0; i < all.length; i++) {
+      all[i].push(str[0]);  
+    }
+  }
+  
+  // for a wildcard, we make a copy of each string set
+  // and for half of them we append a 0 to the string 
+  // and for the other half we append a 1 to the string
+  if (str[0] === '?') {
+    var len = all.length;
+    for (var i = 0; i < len; i++) {
+      var temp = all[i].slice();
+      all.push(temp);
+    }
+    for (var i = 0; i < all.length; i++) {
+      (i < all.length/2) ? all[i].push('0') : all[i].push('1');  
+    }
+  }
+  
+  // recursively calculate all string sets
+  return patterns(str.substring(1), all);
+  
+}
+
+patterns('10?1?', [[]]);
+// [ [ 1, 0, 0, 1, 0 ],  [ 1, 0, 1, 1, 0 ],  [ 1, 0, 0, 1, 1 ],  [ 1, 0, 1, 1, 1 ] ]
+
+
+
