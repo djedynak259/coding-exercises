b2a62a6e61bd661e4c588fe10d32a0e3fec4fce2
djedynak259 <dajed@outlook.com>
2017-07-30 16:57:18 -0700

    roman numberal decoder

--- exercises.js
+++ exercises.js
@@ -2314,3 +2314,17 @@ function solution(number){
   return roman;
 }
 
+// Roman Numeral Decoder
+
+function solution(roman){
+  let lookup = {CM:900,M:1000,CD:400,D:500,XC:90,C:100,XL:40,L:50,IX:9,X:10,IV:4,V:5,I:1}
+  let number = 0;
+  for(i in lookup){
+    while(roman.includes(i)){
+      number += lookup[i]
+      roman = roman.replace(i,'')
+    }
+  }
+  return number;
+}
+
