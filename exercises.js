c0695c955c65063af6879861ef1ce2be637f1e71
djedynak259 <dajed@outlook.com>
2017-07-30 14:59:03 -0700

    pig latin alternate

--- exercises.js
+++ exercises.js
@@ -2293,3 +2293,11 @@ function pigIt(str){
   return arr.join(' ')
 }
 
+// Pig Latin Alternate
+
+function pigIt(str){
+  return str.split(' ').map(function(el){
+    return el.slice(1) + el.slice(0,1) + 'ay';
+  }).join(' ');
+}
+
