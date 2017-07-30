184d0df621bb2e0e63a0c288c1cace52d0b01f08
djedynak259 <dajed@outlook.com>
2017-07-30 15:25:20 -0700

    roman numberal encoder

--- exercises.js
+++ exercises.js
@@ -2301,3 +2301,16 @@ function pigIt(str){
   }).join(' ');
 }
 
+// Roman Numeral Encoder
+
+function solution(number){
+  let lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '';
+  for ( i in lookup ) {
+    while ( number >= lookup[i] ) {
+      roman += i;
+      number -= lookup[i];
+    }
+  }
+  return roman;
+}
+
