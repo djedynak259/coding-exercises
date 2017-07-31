f1c0059f707e740f512eb03fa27d5c3a02721a53
djedynak259 <dajed@outlook.com>
2017-07-31 14:08:10 -0700

    duplicates in array

--- exercises.js
+++ exercises.js
@@ -2444,3 +2444,26 @@ console.log(L1);
 L1.deleteNode(7);
 console.log(L1);
 
+
+// Find duplicates in an Array
+
+function findDuplicates(data) {
+
+  let result = [];
+
+  data.forEach(function(element, index) {
+    
+    // Find if there is a duplicate or not
+    if (data.indexOf(element, index + 1) > -1) {
+      
+      // Find if the element is already in the result array or not
+      if (result.indexOf(element) === -1) {
+        result.push(element);
+      }
+    }
+  });
+
+  return result;
+}
+
+
