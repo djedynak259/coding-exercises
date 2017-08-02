ec3a969675293d63179d1826b389dc5fe2737252
djedynak259 <dajed@outlook.com>
2017-08-02 11:04:09 -0400

    add

--- exercises.js
+++ exercises.js
@@ -2565,7 +2565,53 @@ BinarySearchTree.prototype = {
     //restore constructor
     constructor: BinarySearchTree,
 
-    add: function (value){
+    add: function(value){
+        //create a new item object, place data in
+        var node = {
+                value: value,
+                left: null,
+                right: null
+            },
+
+            //used to traverse the structure
+            current;
+
+        //special case: no items in the tree yet
+        if (this._root === null){
+            this._root = node;
+        } else {
+            current = this._root;
+
+            while(true){
+
+                //if the new value is less than this node's value, go left
+                if (value < current.value){
+
+                    //if there's no left, then the new node belongs there
+                    if (current.left === null){
+                        current.left = node;
+                        break;
+                    } else {
+                        current = current.left;
+                    }
+
+                //if the new value is greater than this node's value, go right
+                } else if (value > current.value){
+
+                    //if there's no right, then the new node belongs there
+                    if (current.right === null){
+                        current.right = node;
+                        break;
+                    } else {
+                        current = current.right;
+                    }       
+
+                //if the new value is equal to the current one, just ignore
+                } else {
+                    break;
+                }
+            }
+        }
     },
 
     contains: function(value){
