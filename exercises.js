ca542ba050693f50794ae164bb54e20adbbd4fa0
djedynak259 <dajed@outlook.com>
2017-08-02 10:55:04 -0400

    contains

--- exercises.js
+++ exercises.js
@@ -2569,6 +2569,28 @@ BinarySearchTree.prototype = {
     },
 
     contains: function(value){
+        var found       = false,
+            current     = this._root
+
+        //make sure there's a node to search
+        while(!found && current){
+
+            //if the value is less than the current node's, go left
+            if (value < current.value){
+                current = current.left;
+
+            //if the value is greater than the current node's, go right
+            } else if (value > current.value){
+                current = current.right;
+
+            //values are equal, found it!
+            } else {
+                found = true;
+            }
+        }
+
+        //only proceed if the node was found
+        return found;
     },
 
     remove: function(value){
