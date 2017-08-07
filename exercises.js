be015f75dfacfc95be282cf9e7c6ae9d073f351e
djedynak259 <dajed@outlook.com>
2017-08-07 14:35:36 -0600

    check if tree is balanced

--- exercises.js
+++ exercises.js
@@ -3239,3 +3239,54 @@ arr.insert(2)
 console.log(arr.getMode());
 
 
+// Check if binary tree is super balanced
+
+  function isBalanced(treeRoot) {
+
+    // a tree with no nodes is superbalanced, since there are no leaves!
+    if (!treeRoot) {
+        return true;
+    }
+
+    var depths = []; // we short-circuit as soon as we find more than 2
+
+    // nodes will store pairs of a node and the node's depth
+    var nodes = [];
+    nodes.push([treeRoot, 0]);
+
+    while (nodes.length) {
+
+        // pop a node and its depth from the top of our stack
+        var nodePair = nodes.pop();
+        var node  = nodePair[0],
+            depth = nodePair[1];
+
+        // case: we found a leaf
+        if (!node.left && !node.right) {
+
+            // we only care if it's a new depth
+            if (depths.indexOf(depth) < 0) {
+                depths.push(depth);
+
+                // two ways we might now have an unbalanced tree:
+                //   1) more than 2 different leaf depths
+                //   2) 2 leaf depths that are more than 1 apart
+                if ((depths.length > 2) ||
+                        (depths.length === 2 && Math.abs(depths[0] - depths[1]) > 1)) {
+                    return false;
+                }
+            }
+
+        // case: this isn't a leaf - keep stepping down
+        } else {
+            if (node.left) {
+                nodes.push([node.left, depth + 1]);
+            }
+            if (node.right) {
+                nodes.push([node.right, depth + 1]);
+            }
+        }
+    }
+
+    return true;
+}
