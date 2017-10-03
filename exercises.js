e8de34c7dddaca97b7b05603629445f0078e7efb
djedynak259 <dajed@outlook.com>
2017-10-03 10:41:35 -0700

    substring max without repeat

--- exercises.js
+++ exercises.js
@@ -4598,4 +4598,42 @@ console.log(myFunction(nroot))
 
 // Substring length
 
+var lengthOfLongestSubstring = function(s) {
+    var max = 0;
+    var str = '';
+    var i = 0;
+    var cache = [];
+    
+    while (i < s.length) {
+        if (cache[s[i]]) {
+            // Found a repeating character.
+            if (str.length > max) {
+                max = str.length;
+            }
+
+            // Not optimal: empty substring, move i back to last position, and start collecting over.
+            /*str = '';
+            // Move back to last non-repeating character.
+            i = cache[s[i]];
+            cache = [];*/
+            
+            // Optimal: strip everything up to the first repeating character in our substring, and continue on.
+            var start = str.indexOf(s[i]);
+            str = str.substring(start + 1);
+        }
+
+        if (i < s.length) {
+            str += s[i];
+            cache[s[i]] = i + 1;
+            i++;
+        }
+    }
+    
+    if (str.length > max) {
+        max = str.length;
+    }
+    
+    return max;
+};
+
 
