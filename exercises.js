0840e045197e5e97233da5e849c206c789490d2e
djedynak259 <dajed@outlook.com>
2017-10-09 19:46:04 -0700

    prime factor n optimal

--- exercises.js
+++ exercises.js
@@ -759,6 +759,41 @@ function testThis(arg, expected) {
 }
 testThis(100,541)
 
+// Really good prime checker
+
+function nthPrime(n) {
+    var P = 0;
+
+    function isPrime(x) {
+        var isPrime= true;
+
+        for (var d = 2; d <= Math.sqrt(x); d++) {
+            if((x/d) % 1 == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
+
+    for (var i = 1; 0 < n; i++) {
+
+        if(isPrime(i)) {
+            P = i; n--;
+        }
+
+        // we can skip the even numbers
+        if(3 <= i){
+            i++;
+        }
+
+    }
+
+    return P;
+}
+
+
 // Palindrome II
 
 function PalindromeTwo(str) { 
