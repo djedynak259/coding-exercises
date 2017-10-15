f24da4c5fdbd004dc2e58ce6c6cf9bee4475a9b8
djedynak259 <dajed@outlook.com>
2017-10-14 20:19:13 -0700

    3 sum closets

--- exercises.js
+++ exercises.js
@@ -5458,3 +5458,59 @@ var threeSum = function(nums) {
     
     return result;
 };
+
+
+// 3sum closest
+
+var threeSumClosest  = function(nums,target) {
+    var result = [nums[0],nums[1],nums[2]];
+    let closestSum = Infinity;
+    
+    if(nums.length < 3){
+        return result;
+    }
+    
+    nums.sort(function(a,b){return a-b});
+    
+    console.log(closestSum)
+    
+    for(let i=0;i<nums.length;i++){
+      
+      let j = i+1;
+      let k = nums.length-1;
+      
+      while(j<k){
+        
+        console.log(nums[i],nums[j],nums[k])
+        
+        let sum = nums[i] + nums[j] + nums[k]
+        let diff = target - sum
+
+        if(diff === 0){
+          return sum
+        }
+        
+        if(sum < target){
+          diff = target-sum
+          j++;
+          while(j < k && nums[j] === nums[j-1]){j++;}
+        } else {
+          diff = sum - target
+          k--
+          while(j < k && nums[k] === nums[k+1]){k--;}   
+        }
+        
+        if(diff < closestSum){
+          console.log('test')
+          closestSum = diff;
+          result = [nums[i],nums[j],nums[k]]
+        }
+      }
+    }
+    
+    return result;
+};
+
+threeSumClosest([1,1,1,-1,2,-4,-1,-1,4],8)
+
+
