f932077690ffed92df4acc8c098d727c02227ffd
djedynak259 <dajed@outlook.com>
2017-11-08 15:03:53 -0800

    unique paths 2

--- exercises.js
+++ exercises.js
@@ -6240,3 +6240,44 @@ var maxSubArray = function(nums) {
     return max
 };
 
+// duynamic programming unique path II
+
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let i,j;
+  
+    for(i=0;i<obstacleGrid.length;i++){
+        for(j=0;j<obstacleGrid[0].length;j++){
+            if(obstacleGrid[i][j] === 0){
+                obstacleGrid[i][j] = '#';
+            }            
+            if(obstacleGrid[i][j] === 1){
+                obstacleGrid[i][j] = 0;
+            }
+        }
+    } 
+    
+    obstacleGrid[0][0] = obstacleGrid[0][0] === '#' ? 1 : 0
+    
+    for(i=1;i<obstacleGrid.length;i++){
+        if(obstacleGrid[i][0] === '#'){
+            obstacleGrid[i][0] = obstacleGrid[i-1][0];
+        }
+    }
+  
+    for(i=1;i<obstacleGrid[0].length;i++){
+        if(obstacleGrid[0][i] === '#'){
+            obstacleGrid[0][i] = obstacleGrid[0][i-1];
+        }
+    }
+  
+    for(i=1;i<obstacleGrid.length;i++){
+        for(j=1;j<obstacleGrid[0].length;j++){
+            if(obstacleGrid[i][j] === '#'){
+                obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+            }            
+        }
+    } 
+  
+    return obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1]
+};
+
